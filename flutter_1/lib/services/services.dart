0371e8f01a45add45b64feeb02f006db6b30ed5a
cow <98164506+YuIYu44@users.noreply.github.com>
2023-12-05 12:01:51 +0800

    Delete flutter_1 directory

--- flutter_1/lib/services/services.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:async';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_1/model/model.dart';
-import 'package:flutter_1/extension/extension.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-part 'auth_services.dart';
-part 'user_services.dart';
